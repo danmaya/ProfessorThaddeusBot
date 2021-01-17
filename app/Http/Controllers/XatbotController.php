5b65831687985dd75372426f61e8f20f6f1e4c34
Daniel Maya <dmaya.calderon@gmail.com>
2021-01-17 23:00:16 +0100

    rolled a nature check

--- app/Http/Controllers/XatbotController.php
+++ app/Http/Controllers/XatbotController.php
@@ -172,7 +172,7 @@ class XatbotController extends Controller
 
             $data = file_get_contents("http://www.recipepuppy.com/api/?i=" . $ingredient);
 
-            if (json_decode($data, true)["results"][0] != null) {
+            if (json_decode($data, true)["results"][0] != "") {
 
                 $title = json_decode($data, true)["results"][0]["title"];
                 $ingredients = json_decode($data, true)["results"][0]["ingredients"];
